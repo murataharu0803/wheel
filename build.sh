2aa72cd683fe6ea2c32cfb404344ba7215c44552
murataharu <murataharu0517@gmail.com>
2025-08-29 04:44:52 +0800

    zip and hint

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 pnpm vite build
 pnpm bun build --compile --target=bun-linux-x64 ./src/index.ts --outfile dist/wheel --minify
 pnpm bun build --compile --target=bun-windows-x64 ./src/index.ts --outfile dist/wheel.exe --minify
+
+zip -r9 dist/windows-x64.zip dist/wheel.exe build
+zip -r9 dist/linux-x64.zip dist/wheel build
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import console from '@/logger'
 import server from '@/server'
 
 server.listen(8888, () => {
-  console.log('Server is running on http://localhost:8888')
+  console.log('伺服器正在運行。')
+  console.log('請在瀏覽器打開儀表版： http://localhost:8888/dashboard')
+  console.log('請在 OBS 輸入計時器瀏覽器來源： http://localhost:8888/timer')
+  console.log('請在 OBS 輸入轉盤瀏覽器來源： http://localhost:8888')
 })
