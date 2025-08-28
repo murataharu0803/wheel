96c710c5de47cc6d7aa3cd41fdd5222bca21f876
murataharu <murataharu0517@gmail.com>
2025-08-29 05:19:18 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# 轉盤
+# 加班轉盤計時器 v2
+基於 [YumeLuckyWheelTimer](https://github.com/kami-0121/wheel) 修改的一般加班轉盤計時器。
+
+## 功能
+- 加班計時器
+- 可自訂化選項隨機轉盤
+- 控制面板
+
+## 使用方法
+1. 至 [Releases](https://github.com/murataharu0803/wheel/releases/tag/v2.0.0) 下載最新版本
+2. 解壓縮並執行 `wheel.exe` (Windows) 或 `wheel` (Linux)
+3. 在 OBS 中新增瀏覽器來源，並輸入以下 URL：
+   - 轉盤：`http://localhost:8888`
+   - 計時器：`http://localhost:8888/timer`
+4. 在瀏覽器打開 [http://localhost:8888/dashboard](http://localhost:8888/dashboard) 進入控制面板
+5. 開始使用轉盤和計時器功能
+
+## 未來規劃
+- i18n 多語系支援
--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 pnpm vite build
-pnpm bun build --compile --target=bun-linux-x64 ./src/index.ts --outfile dist/wheel --minify
-pnpm bun build --compile --target=bun-windows-x64 ./src/index.ts --outfile dist/wheel.exe --minify
+pnpm bun build --compile --target=bun-linux-x64 ./src/index.ts --outfile dist/wheel-linux-x64 --minify
+pnpm bun build --compile --target=bun-windows-x64 ./src/index.ts --outfile dist/wheel-windows-x64.exe --minify
 
-rm -rf dist/windows-x64.zip dist/linux-x64.zip
-zip -r9 dist/windows-x64.zip dist/wheel.exe build
-zip -r9 dist/linux-x64.zip dist/wheel build
+rm -rf dist/wheel-windows-x64.zip dist/wheel-linux-x64.zip
+zip -r9 dist/windows-x64.zip dist/wheel-windows-x64.exe build
+zip -r9 dist/linux-x64.zip dist/wheel-linux-x64 build
 
 rm -rf dist/build
 cp -r build dist/build
